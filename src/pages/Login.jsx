2621077b2ae6ae1c72c1c13e11558494046fc2d1
Alamin-Coding <alamin.codelover@gmail.com>
2025-05-15 15:58:51 +0600

    login and signup page navbar and topbar component design

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,34 @@
+import React from 'react'
+import Input from '../components/Input'
+import Button from '../components/Button'
+import OutlateButton from '../components/OutlineButton'
+import { Link } from 'react-router'
 
 const Login = () => {
   return (
-    <div>
-      login
-    </div>
+    <section>
+        <div className='grid grid-cols-[4fr_3fr] gap-10 items-center'>
+            <div>
+                <img className='w-full' src="/login-image.png" alt="image" />
+            </div>
+            <div className='px-[130px] py-20'>
+                <form action="">
+                  <h2 className='text-[30px]'>Log in to Exclusive</h2>
+                  <p>Enter your details below</p>
+
+                  <div className='grid grid-cols-1 gap-10'>
+                    <Input placeholder={"Name"}/>
+                    <Input placeholder={"Email or Phone Number"} />
+                    <Input placeholder={"Password"} />
+                    <div className='grid grid-cols-2 gap-6'>
+                      <Button TagName={"button"} type='submit'>Login</Button>
+                      <button type='button' className='text-[#DB4444] cursor-pointer'>Forget Password?</button>
+                    </div>
+                  </div>
+                </form>
+            </div>
+        </div>
+    </section>
   )
 }
 
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
+import Input from '../components/Input'
+import Button from '../components/Button'
+import OutlateButton from '../components/OutlineButton'
+import { Link } from 'react-router'
 
 const SignUp = () => {
   return (
-    <div>
-      Sign up
-    </div>
+    <section>
+        <div className='grid grid-cols-[4fr_3fr] gap-10 items-center'>
+            <div>
+                <img className='w-full' src="/login-image.png" alt="image" />
+            </div>
+            <div className='px-[130px] py-20'>
+                <form action="">
+                  <h2 className='text-[30px]'>Create an account</h2>
+                  <p>Enter your details below</p>
+
+                  <div className='grid grid-cols-1 gap-10'>
+                    <Input placeholder={"Name"}/>
+                    <Input placeholder={"Email or Phone Number"} />
+                    <Input placeholder={"Password"} />
+                    <Button TagName={"button"} type='submit'>Create Account</Button>
+                    <OutlateButton TagName={"button"}>
+                      <img src="/google-icon.png" alt="icon" />
+                    Sign up with Google
+                    </OutlateButton>
+                  </div>
+                  <p className='text-gray-700 mt-5'>Already have account? <Link to="/login" className='text-black font-bold'>Log in</Link></p>
+                </form>
+            </div>
+        </div>
+    </section>
   )
 }
 
